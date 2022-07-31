d9c4e3fc61d25cf21c2465765bb4ada9c7ef4148
Jesse Bloom <jbloom@fredhutch.org>
2022-07-31 11:59:03 -0700

    `black` format code and update version to 0.5 (#4)
    * `black` format code and update version to 0.5
    
    * check `black` formatting in Travis test

--- binarymap/__init__.py
+++ binarymap/__init__.py
@@ -12,9 +12,9 @@ into the package namespace:
 
 """
 
-__author__ = '`the Bloom lab <https://research.fhcrc.org/bloom/en.html>`_'
-__email__ = 'jbloom@fredhutch.org'
-__version__ = '0.4'
-__url__ = 'https://github.com/jbloomlab/binarymap'
+__author__ = "`the Bloom lab <https://research.fhcrc.org/bloom/en.html>`_"
+__email__ = "jbloom@fredhutch.org"
+__version__ = "0.5"
+__url__ = "https://github.com/jbloomlab/binarymap"
 
 from binarymap.binarymap import BinaryMap  # noqa: F401
